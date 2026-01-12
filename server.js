9d619c529c9f970efcfb6adb791b7a895f35aa50
Adityaka21 <kashyapaladitya07@gmail.com>
2026-01-13 02:08:17 +0530

    Remove gemini ai

--- server.js
+++ server.js
@@ -38,6 +38,9 @@ app.post("/generate-joining", async (req, res) => {
   }
 });
 
-app.listen(3001, () => {
-  console.log("Backend running on http://localhost:3001");
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Backend running on ${PORT}`);
 });
+
